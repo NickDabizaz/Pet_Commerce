fd03a23fd524ff38ca1aa7b69259e87677362692
Rama <ramaditya32@gmail.com>
2023-11-01 21:12:33 +0700

    upload image sementara
    tolong dicoba dan di perbaiki

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -1,18 +1,67 @@
-const multer = require('multer');
-const Post = require('../models/Post'); 
+const multer = require("multer");
+const fs = require("fs"); //filesystem
+const path = require("path");
+const Post = require('../models/Post');
+
+let id = 1;
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const folderName = Date.now().toString(); 
-    req.folderName = folderName; 
-    cb(null, `./uploads/${folderName}`);
+  destination: (req, file, callback) => {
+    // console.log(req);
+    // console.log(file);
+    const folderName = `uploads/${req.user_id}`;
+
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName, { recursive: true });
+    }
+
+    callback(null, folderName);
   },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
+  filename: (req, file, callback) => {
+    console.log(file);
+    const fileExtension = path.extname(file.originalname).toLowerCase();
+
+    // callback(null, "tes.jpg");
+
+    // memberi nama file sesuai dengan nama file asli yang diupload
+    // callback(null, file.originalname);
+    if (file.fieldname == "image") {
+      callback(null, `${id}${fileExtension}`);
+      id++
+    } else {
+      callback(null, false);
+    }
+  },
 });
 
-const upload = multer({ storage: storage }).single('image'); 
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 50000000, // dalam byte jadi 1000 = 1kb 1000000 = 1mb
+  },
+  fileFilter: (req, file, callback) => {
+    // buat aturan dalam bentuk regex, mengenai extension apa saja yang diperbolehkan
+    const rules = /jpeg|jpg|png|gif/;
+
+    const fileExtension = path.extname(file.originalname).toLowerCase();
+    const fileMimeType = file.mimetype;
+
+    const cekExt = rules.test(fileExtension);
+    const cekMime = rules.test(fileMimeType);
+
+    if (cekExt && cekMime) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+      return callback(
+        new multer.MulterError(
+          "Tipe file harus .gif, .png, .jpg atau .jpeg",
+          file.fieldname
+        )
+      );
+    }
+  },
+});
 
 const getAllPosts = async (req, res) => {
   try {
@@ -35,11 +84,17 @@ const getPostById = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 }
-  
+
 const addPost = async (req, res) => {
-  upload(req, res, async function (err) {
+  req.user_id = req.body.user_id
+  console.log(req.user_id);
+  const uploadingFile = upload.single("image");
+  uploadingFile(req, res, async (err) => {
     if (err) {
-      return res.status(500).json({ message: err.message });
+      console.log(err);
+      return res
+        .status(400)
+        .send((err.message || err.code) + " pada field " + err.field);
     }
 
     const { title, user_id } = req.body;
@@ -47,20 +102,43 @@ const addPost = async (req, res) => {
       return res.status(400).json({ message: 'Title and user_id are required' });
     }
 
-    const image = req.file; 
-    const imagePath = `uploads/${req.folderName}/${image.filename}`;
-
     try {
       const post = await Post.create({
         title: title,
-        image: imagePath,
         user_id: user_id
       });
-      res.json(post);
+      // res.json(post);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
+    return res.status(200).json("successful");
+
   });
+
+  // upload(req, res, async function (err) {
+  //   if (err) {
+  //     return res.status(500).json({ message: err.message });
+  //   }
+
+  //   const { title, user_id } = req.body;
+  //   if (!title || !user_id) {
+  //     return res.status(400).json({ message: 'Title and user_id are required' });
+  //   }
+
+  //   const image = req.file; 
+  //   const imagePath = `uploads/${req.folderName}/${image.filename}`;
+
+  //   try {
+  //     const post = await Post.create({
+  //       title: title,
+  //       image: imagePath,
+  //       user_id: user_id
+  //     });
+  //     res.json(post);
+  //   } catch (error) {
+  //     res.status(500).json({ message: error.message });
+  //   }
+  // });
 }
 
 const updatePost = async (req, res) => {
