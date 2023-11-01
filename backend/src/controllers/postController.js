0f0b67a05bbd4ef67bd4600cd28032849570bfd9
NickDabizaz <94162428+NickDabizaz@users.noreply.github.com>
2023-11-01 22:15:30 +0700

    Update controller

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -1,188 +1,84 @@
-const multer = require("multer");
-const fs = require("fs"); //filesystem
-const path = require("path");
-const Post = require('../models/Post');
+const { Post } = require("../models");
 
-let id = 1;
+const addPost = async (req, res) => {
+  const { title, user_id } = req.body;
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    // console.log(req);
-    // console.log(file);
-    const folderName = `uploads/${req.user_id}`;
+  try {
+    const post = await Post.create({
+      title,
+      user_id
+    });
 
-    if (!fs.existsSync(folderName)) {
-      fs.mkdirSync(folderName, { recursive: true });
-    }
-
-    callback(null, folderName);
-  },
-  filename: (req, file, callback) => {
-    console.log(file);
-    const fileExtension = path.extname(file.originalname).toLowerCase();
-
-    // callback(null, "tes.jpg");
-
-    // memberi nama file sesuai dengan nama file asli yang diupload
-    // callback(null, file.originalname);
-    if (file.fieldname == "image") {
-      callback(null, `${id}${fileExtension}`);
-      id++
-    } else {
-      callback(null, false);
-    }
-  },
-});
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 50000000, // dalam byte jadi 1000 = 1kb 1000000 = 1mb
-  },
-  fileFilter: (req, file, callback) => {
-    // buat aturan dalam bentuk regex, mengenai extension apa saja yang diperbolehkan
-    const rules = /jpeg|jpg|png|gif/;
-
-    const fileExtension = path.extname(file.originalname).toLowerCase();
-    const fileMimeType = file.mimetype;
-
-    const cekExt = rules.test(fileExtension);
-    const cekMime = rules.test(fileMimeType);
-
-    if (cekExt && cekMime) {
-      callback(null, true);
-    } else {
-      callback(null, false);
-      return callback(
-        new multer.MulterError(
-          "Tipe file harus .gif, .png, .jpg atau .jpeg",
-          file.fieldname
-        )
-      );
-    }
-  },
-});
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const getAllPosts = async (req, res) => {
   try {
     const posts = await Post.findAll();
-    res.json(posts);
+    res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-}
+};
 
-const getPostById = async (req, res) => {
-  const postId = req.params.id;
+const getPostById = async (req, res) => {  
+  const { id } = req.params;
+
   try {
-    const post = await Post.findByPk(postId);
+    const post = await Post.findByPk(id);
     if (!post) {
-      return res.status(404).json({ message: 'Post not found' });
+      return res.status(404).json({ message: "Post not found" });
     }
-    res.json(post);
+    res.status(200).json(post);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-}
-
-const addPost = async (req, res) => {
-  req.user_id = req.body.user_id
-  console.log(req.user_id);
-  const uploadingFile = upload.single("image");
-  uploadingFile(req, res, async (err) => {
-    if (err) {
-      console.log(err);
-      return res
-        .status(400)
-        .send((err.message || err.code) + " pada field " + err.field);
-    }
-
-    const { title, user_id } = req.body;
-    if (!title || !user_id) {
-      return res.status(400).json({ message: 'Title and user_id are required' });
-    }
-
-    try {
-      const post = await Post.create({
-        title: title,
-        user_id: user_id
-      });
-      // res.json(post);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-    return res.status(200).json("successful");
-
-  });
-
-  // upload(req, res, async function (err) {
-  //   if (err) {
-  //     return res.status(500).json({ message: err.message });
-  //   }
-
-  //   const { title, user_id } = req.body;
-  //   if (!title || !user_id) {
-  //     return res.status(400).json({ message: 'Title and user_id are required' });
-  //   }
-
-  //   const image = req.file; 
-  //   const imagePath = `uploads/${req.folderName}/${image.filename}`;
-
-  //   try {
-  //     const post = await Post.create({
-  //       title: title,
-  //       image: imagePath,
-  //       user_id: user_id
-  //     });
-  //     res.json(post);
-  //   } catch (error) {
-  //     res.status(500).json({ message: error.message });
-  //   }
-  // });
-}
+};
 
 const updatePost = async (req, res) => {
-  const postId = req.params.id;
+  const { id } = req.params;
   const { title, user_id } = req.body;
-  if (!title || !user_id) {
-    return res.status(400).json({ message: 'Title and user_id are required' });
-  }
 
   try {
-    const [updated] = await Post.update({ title, user_id }, {
-      where: { post_id: postId }
-    });
-    if (updated) {
-      const updatedPost = await Post.findByPk(postId);
-      res.json(updatedPost);
-    } else {
-      res.status(404).json({ message: 'Post not found' });
+    const post = await Post.findByPk(id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
     }
+
+    post.title = title;
+    post.user_id = user_id;
+    await post.save();
+
+    res.status(200).json(post);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-}
+};
 
 const deletePost = async (req, res) => {
-  const postId = req.params.id;
+  const { id } = req.params;
+
   try {
-    const deleted = await Post.destroy({
-      where: { post_id: postId }
-    });
-    if (deleted) {
-      res.json({ message: 'Post deleted' });
-    } else {
-      res.status(404).json({ message: 'Post not found' });
+    const post = await Post.findByPk(id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
     }
+
+    await post.destroy();
+
+    res.status(200).json({ message: "Post deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-}
+};
 
 module.exports = {
+  addPost,
   getAllPosts,
   getPostById,
-  addPost,
   updatePost,
   deletePost
-}
+};
--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,22 +1,12 @@
+// routes/post.js
+
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
-const multer = require('multer');
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads'); // Folder tempat menyimpan file
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname); // Nama file baru
-  }
-});
-
-const upload = multer({ storage: storage });
-
 router.get('/', postController.getAllPosts);
 
-router.post('/',  postController.addPost);
+router.post('/', postController.addPost); 
 
 router.get('/:id', postController.getPostById);
 
