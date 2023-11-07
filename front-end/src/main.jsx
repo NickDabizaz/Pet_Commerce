ba43b86572ebc3242a35e41d07a78b858b233e91
Luambe <melvinaudrey16@gmail.com>
2023-11-07 20:58:33 +0700

    test

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -7,31 +7,44 @@ import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
 import Community from "./pages/Community.jsx";
+import {
+  AboutPage,
+  AlwaysErrorPage,
+  ContactUsPage,
+  ErrorElement,
+  HomePage,
+  MainLayout,
+  NestedPage,
+  NestedPage0,
+  NestedPage1,
+  NestedPage2,
+} from "./Components.jsx";
 
-const routes = [
+const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Homepage />
+    element: <MainLayout />,
+    errorElement: <errorElement />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/contact", element: <ContactUsPage /> },
+      {
+        path: "/nested",
+        // element: <NestedPage />,
+        children: [
+          { path: "page1", element: <NestedPage1 /> },
+          { path: "page2", element: <NestedPage2 /> },
+          { index: true, element: <NestedPage0 /> },
+        ],
+      },
+      {
+        path: "/error",
+        element: <AlwaysErrorPage />,
+        errorElement: <ErrorElement />,
+      },
+    ],
   },
-  {
-    path: "/login",
-    element: <Login />
-  },
-  {
-    path: "/register",
-    element: <Register />
-  },
-  {
-    path: "/admin",
-    element: <AdminDashboard />
-  },
-  {
-    path: "/community",
-    element: <Community />
-  }
-];
-
-const router = createBrowserRouter(routes);
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
