5f9e7767909e828fba7dcab318eb2be1457ed535
Revereign <tenata4321@gmail.com>
2023-11-07 23:17:01 +0700

    Front End Login Fix

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png"
+import word from "../assets/word.png"
+import loginpict from "../assets/loginpict.png"
 
 const Login = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -27,34 +30,38 @@ const Login = () => {
 
   return (
     <div className="container-fluid" style={{ backgroundColor: "#1286CE" }}>
-      <div className="pt-12 pb-12">
-        <div className="container-fluid rounded d-flex" style={{ width: "95%", height: "80vh", backgroundColor: "#FFFFFF" }}>
-          <div className="container-fluid" style={{ width: "100%", height: "80vh", backgroundColor: ""}}>
-            
+      <div className="pt-20 pb-20">
+        <div className="container-fluid rounded d-flex shadow" style={{ width: "90%", height: "80vh", backgroundColor: "#6CD4FF", overflow: "hidden"}}>
+          <div className="container-fluid rounded" style={{ width: "100%", height: "75vh", position: "relative"}}>
+            <img src={logo} style={{width: "30vh", height: "30vh", left: -10, position: "absolute", zIndex: 2}}/>       
+            <img src={loginpict} className="rounded" style={{width: "120vh", height: "35.75rem", left: -15, position: "absolute", zIndex: 1}}/>       
           </div>
-          <div className="container-fluid" style={{ width: "100%", height: "80vh", backgroundColor: "FFFFFF"}}>
-            <h2>Login</h2>
+          <div className="container-fluid rounded" style={{ width: "60%", height: "75vh", marginTop: "2.5vh", backgroundColor: "#FFFFFF"}}>
+          <img src={word} style={{ display: "block", margin: "0 auto", width: "12rem", height: "6rem", marginTop: "4%" }} />           
             <form onSubmit={handleSubmit(onSubmit)}>
               <div>
-                <label>Email:</label>
-                <input
+                <label style={{ marginLeft: "3%", fontWeight: 700, marginBottom: "1%", marginTop: "3%"}}>Email</label>
+                <input className="form-control" style={{ marginLeft: "3%", width: "94%"}}
                   type="email"
                   {...register("email", { required: "Email is required" })}
                 />
                 {errors.email && <p>{errors.email.message}</p>}
               </div>
               <div>
-                <label>Password:</label>
-                <input
+                <label style={{marginTop: "7%", marginLeft: "3%", fontWeight: 700, marginBottom: "1%"}}>Password</label>
+                <input className="form-control" style={{ marginLeft: "3%", width: "94%"}}
                   type="password"
                   {...register("password", { required: "Password is required" })}
                 />
                 {errors.password && <p>{errors.password.message}</p>}
               </div>
-              <button type="submit">Login</button>
+              <button type="submit" 
+              className="btn btn-info" 
+              style={{backgroundColor: "#C46E85", borderColor: "#C46E85", marginTop: "8%", marginLeft: "3%", fontWeight: 700, width: "94%", height:"2.5rem", color: "white"}}
+              >Login</button>
             </form>
-            <p>
-              Belum punya akun? <Link to="/register">Daftar di sini</Link>
+            <p className="text-center" style={{marginTop: "8%"}}>
+              Don't Have Any Account? <Link to="/register"><b style={{color: "#D39C39"}}>Register Here</b></Link>
             </p>
             {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
           </div>
