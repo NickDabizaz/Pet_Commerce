8d662095be4a31e5da0811d6cc996d6c9c1930c4
Luambe <melvinaudrey16@gmail.com>
2023-11-08 20:55:11 +0700

    profile 20%

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -7,7 +7,6 @@ import { useCookies } from "react-cookie";
 function Profile() {
   const [cookie, setCookie] = useCookies("user_id");
   const [response, setResponse] = useState([]);
-  console.log(cookie);
 
   useEffect(() => {
     axios
@@ -19,35 +18,84 @@ function Profile() {
         console.error("Error fetching data:", error);
       });
   }, []);
-  //   console.log(response);
+  console.log(response);
 
   return (
     <>
       <MainLayout />
-      <div className="text-center">
-        <h1>Profile</h1>
-        <div
-          className="m-auto"
-          style={{
-            width: "10rem",
-            height: "10rem",
-            objectFit: "cover",
-          }}
-        >
-          <img
-            src={dogo}
+      <div className=" m-5 bg-warning">
+        <h1 style={{ fontSize: "2rem" }}>My Profile</h1>
+        <h1 style={{ fontSize: "1.5rem" }}>Manage your account</h1>
+        <div className="mt-5" style={{ display: "flex" }}>
+          <div className="m-auto" style={{ width: "50rem" }}>
+            <form>
+              <table className="table text-start">
+                <tr>
+                  <td className="text-end" style={{ width: "10rem" }}>
+                    Nama:
+                  </td>
+                  <td>
+                    <input
+                      className="form-control"
+                      type="text"
+                      placeholder={response.name}
+                    ></input>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-end">Email:</td>
+                  <td>
+                    <input
+                      className="form-control"
+                      type="text"
+                      placeholder={response.email}
+                    ></input>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-end">Address:</td>
+                  <td>
+                    <input
+                      className="form-control"
+                      type="text"
+                      placeholder={response.address}
+                    ></input>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-end">Phone Number:</td>
+                  <td>
+                    <input
+                      className="form-control"
+                      type="text"
+                      placeholder={response.phone_number}
+                    ></input>
+                  </td>
+                </tr>
+              </table>
+            </form>
+          </div>
+          <div></div>
+          <div
+            className="m-auto"
             style={{
+              width: "10rem",
               height: "10rem",
               objectFit: "cover",
-              borderRadius: "50%  ",
             }}
-          />
+          >
+            <img
+              src={dogo}
+              style={{
+                height: "10rem",
+                objectFit: "cover",
+                borderRadius: "50%  ",
+              }}
+            />
+          </div>
         </div>
-        <div>Nama: {</div>
-        <div>Email</div>
-        <div>Address</div>
-        <div>Phone Number</div>
       </div>
+      <div>List Toko:</div>
     </>
   );
 }
