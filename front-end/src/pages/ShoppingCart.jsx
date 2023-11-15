86cfe4b8f9becdd256299cdad016bac0cbc161d5
Revereign <tenata4321@gmail.com>
2023-11-15 23:17:50 +0700

    Front end Cart & beberapa fungsinya

--- front-end/src/pages/ShoppingCart.jsx
+++ front-end/src/pages/ShoppingCart.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useCookies } from "react-cookie";
 import { Card, Button } from "react-bootstrap";
 import { MainLayout } from "../Components";
+import trash from "../assets/trash.png"
 
 const ShoppingCart = () => {
   const [cartData, setCartData] = useState({ cartItems: [], total: 0 });
@@ -45,45 +46,100 @@ const ShoppingCart = () => {
   }
 
   return (
-    <div>
+    <div style={{ backgroundColor: "#F3F0F0", height: "1000px" }}>
       <MainLayout />
-      <div style={{ margin: "1vh 5vw 1vw 5vw", fontSize: "1rem" }}>
-        {cartData.cartItems.map((item) => (
-          <Card
-            key={item.cart_id}
-            style={{ width: "90vw", marginBottom: "10px" }}
-          >
-            <div style={{ display: "flex" }}>
-              <div style={{ width: "fit-content" }}>
+      <p className="text-center" style={{ marginTop: "0.5%", fontFamily: "Literata", fontSize: "24pt", fontWeight: 600 }}>My Cart</p>
+      <div className="overflow-y-auto" style={{ width: "90%", marginLeft: "5%", marginTop: "1%", height: "650px", marginBottom: "20px", }}>
+        <div>
+          {cartData.cartItems.map((item) => (
+            <div className="d-flex"
+              key={item.cart_id}
+              style={{ width: "100%", marginBottom: "10px", backgroundColor: "#FFFFFF" }}
+            >
+              <div className="rounded-start" style={{ width: "auto" }}>
                 <img
+                  className="rounded-start"
                   src="http://via.placeholder.com/640x360"
-                  style={{ minHeight: "8rem", maxHeight: "8rem" }}
+                  style={{ minHeight: "8rem", maxHeight: "8rem", minWidth: "auto" }}
                 />
               </div>
-              <div style={{ marginLeft: "1vw" }}>
-                <Card.Title>{item.product_name}</Card.Title>
-                <Card.Text>
-                  Price: Rp {item.price} <br />
-                  Subtotal: {item.subtotal}
-                </Card.Text>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => updateCart(item.product_id, item.qty - 1)}
-                >
-                  -
-                </button>
-                {item.qty}
-                <button
-                  className="btn btn-success"
-                  onClick={() => updateCart(item.product_id, item.qty + 1)}
-                >
-                  +
-                </button>
+              <div style={{ display: "flex", marginLeft: "1.5%", marginTop: "0.5%", marginBottom: "0.5%" }}>
+                <div style={{ minWidth: "930px" }}>
+                  <p style={{ fontFamily: "Literata", fontSize: "18pt", fontWeight: 600, marginTop: "0.5%" }}>{item.product_name}</p>
+                  <p style={{ fontFamily: "Literata", fontSize: "20pt", fontWeight: 600, color: "#FF4040", marginTop: "3%" }}>Rp {item.subtotal}</p>
+                </div>
+                <div style={{ marginLeft: "2%", marginTop: "0.5%", marginBottom: "0.5%", minWidth: "150px" }}>
+                  <img className="cursor-pointer" src={trash} style={{ width: "25px", height: "25px", marginRight: 25, marginLeft: "auto" }} />
+                  <div className="d-flex" style={{ marginRight: 0, marginLeft: "15%", marginTop: "25%" }}>
+                    <button
+                      className="rounded-start"
+                      style={{
+                        height: "2rem",
+                        backgroundColor: "#E5E5E5",
+                        width: "30px",
+                        color: "#777777"
+                      }}
+                      onClick={() => updateCart(item.product_id, item.qty - 1)}
+                    >
+                      -
+                    </button>
+                    <input                      
+                      className="form-control text-center"
+                      style={{
+                        height: "2rem",
+                        width: "50px",
+                        borderRadius: "0%"
+                      }}
+                      value={item.qty}
+                    />
+                    <button
+                      className="rounded-end"
+                      style={{
+                        height: "2rem",
+                        backgroundColor: "#E5E5E5",
+                        width: "30px",
+                        color: "#777777"
+                      }}
+                      onClick={() => updateCart(item.product_id, item.qty + 1)}
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+                <div className="d-flex" style={{ marginLeft: "1vw", marginTop: "1%" }}>
+
+
+                </div>
               </div>
             </div>
-          </Card>
-        ))}
-        <h2>Total: {cartData.total}</h2>
+          ))}
+        </div>
+      </div>
+      <div>
+        <div className="container-fluid rounded" style={{ marginLeft: "5%", width: "90%", height: "7rem", backgroundColor: "#FFFFFF" }}
+        >
+          <div className="d-flex" style={{ marginLeft: "3%", width: "94%" }}>
+            <p style={{ marginTop: "1%", fontFamily: "Literata", fontSize: "16pt", fontWeight: 600 }}>Total: </p>
+            <p style={{ marginTop: "1%", marginRight: 0, marginLeft: "auto", fontFamily: "Literata", fontSize: "16pt", fontWeight: 600 }}>Rp {cartData.total}</p>
+          </div>
+          <button
+            type="button"
+            className="btn btn-info"
+            style={{
+              backgroundColor: "#1286CE",
+              borderColor: "#1286CE",
+              marginTop: "1%",
+              marginLeft: "3%",
+              fontWeight: 700,
+              fontSize: "14pt",
+              width: "94%",
+              height: "2.5rem",
+              color: "white",
+            }}
+          >
+            Checkout
+          </button>
+        </div>
       </div>
     </div>
   );
