cbe4238837ba7015d0054542f17fb077c4055bbd
NickDabizaz <94162428+NickDabizaz@users.noreply.github.com>
2023-11-22 17:52:46 +0700

    masih bikin config midtrans

---
+++ backend/src/config/midtransConfig.js
@@ -0,0 +1,10 @@
+// // midtransConfig.js
+// const { Client } = require('midtrans-client');
+
+// const midtransClient = new Client({
+//   isProduction: false, // ganti menjadi true saat di production
+//   serverKey: 'YOUR_SERVER_KEY',
+//   clientKey: 'YOUR_CLIENT_KEY',
+// });
+
+// module.exports = midtransClient;
